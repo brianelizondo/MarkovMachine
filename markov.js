b55d38c78f03bef244ad73f38626cea28ca2a854
Brian Elizondo <brian_elizondo@msn.com>
2022-06-12 10:35:48 -0400

    Step 1: Implement the Markov Machine

--- markov.js
+++ markov.js
@@ -1,29 +1,68 @@
 /** Textual markov chain generator */
 
+function randomWord(options_arr){
+    let random_idx = Math.floor(Math.random() * options_arr.length);
+    return options_arr[random_idx];
+}
 
 class MarkovMachine {
+    /** build markov machine; read in text.*/
+    constructor(text) {
+        let words = text.split(/[ \r\n]+/);
+        this.words = words.filter(c => c !== "");
+        this.makeChains();
+    }
 
-  /** build markov machine; read in text.*/
+    /** set markov chains:
+    *
+    *  for text of "the cat in the hat", chains will be
+    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+    makeChains() {
+        // TODO
+        let chains = new Map();
+        let words_arr_length = this.words.length;
 
-  constructor(text) {
-    let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
-    this.makeChains();
-  }
+        for(let i = 0; i < words_arr_length; i++){
+            let current_word = this.words[i];
+            let next_words = [];
+            
+            let idx_new = i;
+            while(idx_new != -1){
+                if((idx_new + 1) == words_arr_length){
+                    next_words.push(null);
+                }else{
+                    next_words.push(this.words[idx_new + 1]);
+                }
+                idx_new = this.words.indexOf(current_word, idx_new + 1);
+            }
+            
+            if(!chains.has(current_word)){
+                chains.set(current_word, next_words);
+            }
+        }
 
-  /** set markov chains:
-   *
-   *  for text of "the cat in the hat", chains will be
-   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+        this.chains = chains;  
+    } 
 
-  makeChains() {
-    // TODO
-  }
+    /** return random text from chains */
+    makeText(numWords = 100) {
+        // TODO
+        let random_text = "";
+        let words_count = 0;
+        let basic_words = Array.from(this.chains.keys());
+        let current_word = randomWord(basic_words);
+        
+        while(words_count < numWords && current_word !== null){
+            words_count++;
+            random_text += current_word + " ";
+            current_word = randomWord(this.chains.get(current_word));
+        }
 
-
-  /** return random text from chains */
-
-  makeText(numWords = 100) {
-    // TODO
-  }
+        return random_text.trim();
+    }
 }
+
+
+let mm = new MarkovMachine("the cat in the hat");
+console.log(mm);
+console.log(mm.makeText(numWords=50));
